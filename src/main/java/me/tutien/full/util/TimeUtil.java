268e041894dd8008e43ab434babaf59624f62352
hungban946-prog <hungban946@gmail.com>
2026-02-27 14:00:39 +0700

    Add TimeUtil class for time-related utilities

--- src/main/java/me/tutien/full/util/TimeUtil.java
+++ src/main/java/me/tutien/full/util/TimeUtil.java
@@ -1,1 +1,11 @@
+package me.tutien.full.util;
 
+public final class TimeUtil {
+  private TimeUtil() {}
+  public static long now() { return System.currentTimeMillis(); }
+  public static long secLeft(long untilMs) {
+    long left = untilMs - now();
+    if (left <= 0) return 0;
+    return (long) Math.ceil(left / 1000.0);
+  }
+}
