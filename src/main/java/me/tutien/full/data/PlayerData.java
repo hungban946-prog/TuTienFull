f9e79d492a8f468cde8c5aa2c50154dcd7e6f8cb
hungban946-prog <hungban946@gmail.com>
2026-02-27 14:03:12 +0700

    Add PlayerData class with realm and progression methods

--- src/main/java/me/tutien/full/data/PlayerData.java
+++ src/main/java/me/tutien/full/data/PlayerData.java
@@ -1,1 +1,16 @@
+package me.tutien.full.data;
 
+import me.tutien.full.system.Progression;
+import me.tutien.full.system.Realm;
+
+public final class PlayerData {
+  public Realm realm = Realm.LUYEN_KHI;
+  public int layer = 1;
+  public int exp = 0;
+
+  public long trainCooldownUntil = 0L;
+  public long breakCooldownUntil = 0L;
+
+  public int need() { return Progression.expNeed(realm, layer); }
+  public long power() { return Progression.power(realm, layer, exp); }
+}
