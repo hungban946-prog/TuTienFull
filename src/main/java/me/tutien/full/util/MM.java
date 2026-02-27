d334f5b01eee39e59aebbcd3a74bb87969c64b4e
hungban946-prog <hungban946@gmail.com>
2026-02-27 13:59:57 +0700

    Add MM class for MiniMessage handling

--- src/main/java/me/tutien/full/util/MM.java
+++ src/main/java/me/tutien/full/util/MM.java
@@ -1,1 +1,13 @@
+package me.tutien.full.util;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+public final class MM {
+  private static final MiniMessage MM = MiniMessage.miniMessage();
+  private MM() {}
+
+  public static Component c(String mini) {
+    return MM.deserialize(mini == null ? "" : mini);
+  }
+}
